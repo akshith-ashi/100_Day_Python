529728f5446035954fd606b6a183833f9e9caf7c
akshith-ashi <akshithashi07@gmail.com>
2026-01-12 20:44:32 +0530

    initial commit

---
+++ Day1.py
@@ -0,0 +1,4 @@
+print("Welcome to The Band Name Generator")
+city = input("Which City do you grow up in? ")
+pet = input("What is the name of your favourite pet? ")
+print("Your Band Name could be: "+city+" "+pet)
